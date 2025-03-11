7ba61c9933c64568768cad8f834c35a1bcd2d50a
KattarCoder <43518530+sagarpjpt@users.noreply.github.com>
2025-03-11 16:28:23 +0530

    Priority Queue
    The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.

--- FindMedianfromDataStream.java
+++ FindMedianfromDataStream.java
@@ -1,29 +1,46 @@
 class MedianFinder {
-    private PriorityQueue<Integer> maxHeap; // Left half (smaller numbers)
-    private PriorityQueue<Integer> minHeap; // Right half (larger numbers)
+
+    //maxheap for the smaller half ie [1 2 3 4 5 6 7 8] so maxheap stores [1 2 3 4]
+    private PriorityQueue<Integer> max_heap;
+    //min heap will store [5 6 7 8]
+    private PriorityQueue<Integer> min_heap;
 
     public MedianFinder() {
-        maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // Max-Heap
-        minHeap = new PriorityQueue<>(); // Min-Heap
-    }
+        
+        max_heap = new PriorityQueue<>((a, b) -> b-a);//lembda exp
+        min_heap = new PriorityQueue<>();
 
+    }
+    
     public void addNum(int num) {
-        // Step 1: Add to maxHeap (left half)
-        maxHeap.offer(num);
+        
+        if(max_heap.isEmpty() || num <= max_heap.peek())
+            max_heap.offer(num);
+        else
+            min_heap.offer(num);
 
-        // Step 2: Balance - Ensure maxHeap's top is smaller than minHeap's top
-        minHeap.offer(maxHeap.poll());
+        //need to balance heap with atmost 1 extra ele allow in maxheap
 
-        // Step 3: Maintain size balance - maxHeap should have more or equal elements
-        if (maxHeap.size() < minHeap.size()) {
-            maxHeap.offer(minHeap.poll());
-        }
+        if(max_heap.size() > min_heap.size() + 1)
+            min_heap.offer(max_heap.poll());
+        else if(min_heap.size() > max_heap.size())
+            max_heap.offer(min_heap.poll());
     }
-
+    
     public double findMedian() {
-        if (maxHeap.size() > minHeap.size()) {
-            return maxHeap.peek(); // Odd count, median is top of maxHeap
-        }
-        return (maxHeap.peek() + minHeap.peek()) / 2.0; // Even count, avg of tops
+
+        //if size of array odd
+        if(max_heap.size() > min_heap.size())
+            return max_heap.peek();
+        //size of array even
+        else
+            return (max_heap.peek() + min_heap.peek()) / (double)2;
     }
 }
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
